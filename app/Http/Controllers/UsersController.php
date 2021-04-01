500bc30677b627a5af8757e32563476dfafd1591
村上晴 <hkw355@gmai.com>
2021-04-01 09:04:37 +0000

    fix

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,11 +11,6 @@ use App\Models\Follower;
 
 class UsersController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index(User $user)
     {
         $all_users = $user->getAllUsers(auth()->user()->id);
@@ -51,35 +46,6 @@ class UsersController extends Controller
         }
     }
     
-    
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show(User $user, Tweet $tweet, Follower $follower)
     {
         $login_user = auth()->user();
@@ -100,46 +66,15 @@ class UsersController extends Controller
             'follower_count' => $follower_count
         ]);
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function edit(User $user)
     {
         return view('users.edit', ['user' => $user]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, User $user)
     {
-        /*
         $data = $request->all();
-        if ($request->remove == 'true') {
-            $data['profile_image'] = null;
-        } elseif ($request->file('profile_image')) {
-            $path = $request->file('profile_image')->store('public/storage');
-            $data['profile_image'] = basename($path);
-        } else {
-            $data['profile_image'] = $user->profile_image;
-        }
-        */
-        
-        $data = $request->all();
-        $path = $request->file('profile_image')->store('public/profile_image');
-        $data['profile_image'] = basename($path);
-        
-        // 該当するデータを上書きして保存する
-        $user->fill($data)->save();
-        
         $validator = Validator::make($data, [
             'screen_name'   => ['required', 'string', 'max:50', Rule::unique('users')->ignore($user->id)],
             'name'          => ['required', 'string', 'max:255'],
@@ -148,18 +83,7 @@ class UsersController extends Controller
         ]);
         $validator->validate();
         $user->updateProfile($data);
-        
-        return redirect('users/'.$user->id);
-    }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        return redirect('users/'.$user->id);
     }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,8 +12,6 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
             $table->increments('id');
             $table->string('screen_name')->unique()->null()->comment('アカウント名');
             $table->string('name')->null()->comment('ユーザ名');
